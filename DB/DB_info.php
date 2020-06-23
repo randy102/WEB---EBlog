5a2d92a3bf61130509ea3e479c309c5f7e627449
Randy Liu <welldey102@gmail.com>
2020-06-23 09:52:59 +0700

    Change DB

--- DB/DB_info.php
+++ DB/DB_info.php
@@ -1,9 +1,11 @@
 <?php
-define("DB_HOST","sql12.freemysqlhosting.net");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-define("DB_USER_NAME","sql12350217");
+define("DB_HOST",$url["host"]);
 
-define("DB_PASSWORD","BdNdIjGTze");
+define("DB_USER_NAME",$url["user"]);
 
-define("DB_NAME","sql12350217");
+define("DB_PASSWORD",$url["pass"]);
+
+define("DB_NAME",substr($url["path"], 1));
 ?>
